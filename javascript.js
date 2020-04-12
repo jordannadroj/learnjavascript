c9cc2550bd67083645f08d00e39093a40af55b8a
jordannadroj <jordansiemens93@gmail.com>
2020-04-12 22:10:32 +0200

    this is my first commit

---
+++ javascript.js
@@ -0,0 +1,6 @@
+var greeting = "hello"; // string variable
+
+console.log(greeting)
+
+var age = 26; // int = integer variable 
+
---
+++ notes.js
@@ -0,0 +1,37 @@
+//make a comment with two slashes
+// these are the commands that Nathan has taught us today. 
+
+/* to do a multi line comment use slash asterisk on top line then asterisk slash on end line
+
+*/
+
+
+/* Terminal commands:
+mkdir: make directory
+
+ls: this means list all the filed inside the folder
+
+touch: this creates a file on any simple type
+
+*/
+
+/* Node commands:
+
+node --version: tells you which version of node you have installed
+
+*/
+
+
+/* Javascript definitions:
+
+var myVariableName = "string"
+
+*/ 
+
+/* Git:
+What is git? Git is version control
+Its like dropbox for computer programs. A way to share code
+Learning Git is super fuckin important!!!!!!!! 
+Also a great way to start your own fun projects 
+
+*/
